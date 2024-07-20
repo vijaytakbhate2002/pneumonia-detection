31c2c6050856629ab0634addccdd9f9af2cec7fd
Vijay Takbhate <vijaytakbhate20@gmail.com>
2024-07-20 15:15:06 +0530

    third commit

--- Prediction.py
+++ Prediction.py
@@ -8,13 +8,13 @@ warnings.filterwarnings('ignore')
 
 class Predict:
     LABELS = {0:"Bacterial pneumonia", 1:"Normal", 2:"Virus pneumonia"}
-    model = load_model("pneumonia-detection\\static\\CNN_32_32_1_ASA.h5")
+    model = load_model("static\\CNN_32_32_1_ASA.h5")
 
     def __init__(self,arr:np.ndarray)-> None:
         """ Args: np.ndarray
             Return: None"""
         self.arr = arr
-        
+
     def predict(self)-> np.ndarray:
         """ Args: None 
             Return: str (bacterial, virul or normal)"""
